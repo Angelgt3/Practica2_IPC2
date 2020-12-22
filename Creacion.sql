3433279c01259bce5520df7ff30a1bd35141641c
Angelgt3 <angelaragon130@gmail.com>
2020-12-22 12:32:54 -0600

    Creacion de la db

---
+++ Creacion.sql
@@ -0,0 +1,55 @@
+drop database Hoteles;
+create database Hoteles;
+Use Hoteles;
+
+create table Hotel(
+IdHotel int auto_increment primary key,
+CantidadDisponible int,
+Pais varchar(50),
+Estrellas int
+);
+
+create table Cliente(
+IdCliente int auto_increment primary key,
+Nombre varchar(50),
+Apellido varchar(50)
+);
+
+create table Habitacion(
+IdHabitacion int auto_increment primary key,
+Tipo varchar(50)
+);
+
+CREATE TABLE Checkin (
+    IdIn INT AUTO_INCREMENT PRIMARY KEY,
+    Fecha DATE,
+    Hora TIME
+);
+
+CREATE TABLE Checkon (
+    IdOut INT AUTO_INCREMENT PRIMARY KEY,
+    Fecha DATE,
+    Hora TIME
+);
+
+create table Reservacion(
+IdReservacion int auto_increment primary key,
+IdHabitacion int,
+IdCliente int,
+IdHotel int,
+Fecha date,
+foreign key(IdHabitacion) references Habitacion(IdHabitacion),
+foreign key(IdCliente) references Cliente(IdCliente),
+foreign key(IdHotel) references Hotel(IdHotel)
+);
+
+create table Registro(
+IdRegistro int auto_increment primary key,
+IdReservacion int,
+IdIn int,
+IdOut int,
+Mora int,
+foreign key(IdReservacion) references Reservacion(IdReservacion),
+foreign key(IdIn) references Checkin(IdIn),
+foreign key(IdOut) references Checkon(IdOut)
+);
