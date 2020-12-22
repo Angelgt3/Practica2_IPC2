c0c4c9fc5b180feacb3a84a78aaea611c0481b2c
Angelgt3 <angelaragon130@gmail.com>
2020-12-22 15:35:26 -0600

    Insert completo

--- Creacion.sql
+++ Creacion.sql
@@ -23,18 +23,6 @@ IdHotel int,
 foreign key(IdHotel) references Hotel(IdHotel)
 );
 
-CREATE TABLE Checkin (
-    IdIn INT AUTO_INCREMENT PRIMARY KEY,
-    Fecha DATE,
-    Hora TIME
-);
-
-CREATE TABLE Checkon (
-    IdOut INT AUTO_INCREMENT PRIMARY KEY,
-    Fecha DATE,
-    Hora TIME
-);
-
 create table Reservacion(
 IdReservacion int auto_increment primary key,
 IdHabitacion int,
@@ -49,10 +37,8 @@ foreign key(IdHotel) references Hotel(IdHotel)
 create table Registro(
 IdRegistro int auto_increment primary key,
 IdReservacion int,
-IdIn int,
-IdOut int,
+CheckIn time,
+CheckOut time,
 Mora int,
-foreign key(IdReservacion) references Reservacion(IdReservacion),
-foreign key(IdIn) references Checkin(IdIn),
-foreign key(IdOut) references Checkon(IdOut)
+foreign key(IdReservacion) references Reservacion(IdReservacion)
 );
